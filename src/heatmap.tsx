84574e0740d6a70620e1aa593dd7521158eb1e95
zakhij <zakhijaouy@gmail.com>
2024-10-09 11:46:43 -0700

    Heatmap UI: Scaling, Legend, Tooltip

--- src/heatmap.tsx
+++ src/heatmap.tsx
@@ -1,60 +1,92 @@
 import CalHeatmap from 'cal-heatmap';
 import Tooltip from 'cal-heatmap/plugins/Tooltip';
 import 'cal-heatmap/cal-heatmap.css';
+import LegendLite from 'cal-heatmap/plugins/LegendLite';
+import Legend from 'cal-heatmap/plugins/Legend';
 import { ActivityData } from './types';
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-
-
-const Heatmap: React.FC<{ data: ActivityData }> = ({ data }) => {
+const Heatmap: React.FC<{ data: ActivityData}> = ({ data}) => {
     const [cal, setCal] = useState<CalHeatmap | null>(null);
 
     useEffect(() => {
         const newCal = new CalHeatmap();
         setCal(newCal);
         const dataArray = Object.entries(data).map(([date, value]) => ({ date, value }));
+        const maxValue = Math.max(...dataArray.map(item => item.value));
 
-  
-
+        // Calculate the start date (1 year ago from today)
+        const startDate = new Date();
+        startDate.setFullYear(startDate.getFullYear() - 1);
         newCal.paint(
             {
                 data: { source: dataArray, x: 'date', y: 'value' },
-                date: { start: new Date('2024-01-01') },
-                range: 12,
+                date: { start: startDate},
+                range: 13,
                 scale: {
                     color: {
                         type: 'threshold',
                         range: ['#14432a', '#166b34', '#37a446', '#4dd05a'],
-                        domain: [1, 3, 5],
+                        domain: [0, Math.floor(maxValue / 3), Math.floor((2 * maxValue) / 3), maxValue],
                     },
                 },
                 domain: {
                     type: 'month',
-                    //gutter: 4,
-                    //label: { text: 'MMM', textAlign: 'start', position: 'top'  
-                    //},
+                    gutter: 3,
+                    label: { text: 'MMM', textAlign: 'start', position: 'top' },
                 },
-                subDomain: { type: 'day', 
-                  radius: 2, 
-                  //width: 11, 
-                  //height: 11, 
-                  //gutter: 4 
+                subDomain: { 
+                    type: 'ghDay',
+                    radius: 2,
+                    width: 8,
+                    height: 8,
+                    gutter: 3
                 },
                 itemSelector: '#heatmap-container',
             },
             [
-              [
-                Tooltip,
-              ],
+                [
+                    Tooltip,
+                    {
+                        text: function(date: any, value: any, dayjsDate: any) {
+
+                            return value ? `${value} contributions on ${dayjsDate.format('MMMM D, YYYY')}` : `No contributions on ${dayjsDate.format('MMMM D, YYYY')}`;
+                        }
+                    }
+                ],
+                [
+                    LegendLite,
+                    {
+                        itemSelector: '#heatmap-legend',
+                        label: 'Contributions',
+                        includeBlank: true,
+                    }
+                ],
             ]
         );
+
+
 
         return () => {
             newCal.destroy();
         };
     }, [data]);
 
-    return <div id="heatmap-container"></div>;
+    return (
+        <div>
+            <div id="heatmap-container"></div>
+            <div style={{ float: 'left', fontSize: 11, marginTop: '5px' }}>
+            Less
+            <div
+                id="heatmap-legend"
+                style={{ display: 'inline-block', margin: '0 8px' }}
+            ></div>
+            More
+            </div>
+
+
+        </div>
+    );
 }
 
 export default Heatmap;
